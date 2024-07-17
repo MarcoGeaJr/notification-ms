46209b2faa24cc590c4bfd46fa845ffe36d80eff
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-07-16 23:11:29 -0300

    feat: start with gRPC

---
+++ src/Snotify.API/Grpc/NotificationScheduleService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace Snotify.API.Grpc;
+
+public class NotificationScheduleService : NotificationSchedule.NotificationScheduleBase
+{
+	public override Task<NotificationStatusResponse> GetNotificationStatus(GetNotificationStatusRequest request, ServerCallContext context)
+	{
+		// try get from cache
+
+		// try get from db
+
+		// not found exception
+
+		// save on cache
+
+		// return
+
+		return base.GetNotificationStatus(request, context);
+	}
+}
--- src/Snotify.API/Program.cs
+++ src/Snotify.API/Program.cs
@@ -1,51 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Snotify.Application.Services.Notifiers;
-using Snotify.Application.Services.Notifiers.EmailNotifiers;
+using Snotify.API.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Logging.AddConsole();
+//builder.AddBasicServiceDefaults();
+//builder.AddApplicationServices();
 
-builder.Services.AddKeyedSingleton<INotifier, EmailNotifierV1>("email:v1");
-builder.Services.AddKeyedSingleton<INotifier, EmailNotifierV2>("email:v2");
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+//app.MapDefaultEndpoints();
 
-app.UseHttpsRedirection();
-
-app.MapGet("/v1/email", ([FromServices] IServiceProvider provider) =>
-{
-	var version = 1;
-	Snotify.Domain.ValueObjects.Channel channel = Snotify.Domain.ValueObjects.Channel.EMAIL;
-
-	var test = provider.GetRequiredKeyedService<INotifier>($"{channel.Name.ToLower()}:v{version}");
-
-	return Results.Ok();
-})
-.WithName("GetEmailV1")
-.WithOpenApi();
-
-app.MapGet("/v2/email", ([FromServices] IServiceProvider provider) =>
-{
-	var version = 2;
-	Snotify.Domain.ValueObjects.Channel channel = Snotify.Domain.ValueObjects.Channel.EMAIL;
-
-	var test = provider.GetRequiredKeyedService<INotifier>($"{channel.Name.ToLower()}:v{version}");
-
-	return Results.Ok();
-})
-.WithName("GetEmailV2")
-.WithOpenApi();
+app.MapGrpcService<NotificationScheduleService>();
 
 app.Run();
